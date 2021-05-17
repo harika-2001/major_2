df24b4e87fe8dfac657a01ca56cd5dcecc1ca376
harika-2001 <65670592+harika-2001@users.noreply.github.com>
2021-05-17 17:26:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+function myfunction_1(){
+  document.getElementById("collapse_1").style.display="block";
+  document.getElementById("three").style.height="45vw";
+  document.getElementById("collapse_2").style.display="none";
+  document.getElementById("collapse_3").style.display="none";
+  document.getElementById("collapse_4").style.display="none";
+  document.getElementById("collapse_5").style.display="none";
+  document.getElementById("player").style.display="none";
+}
+function myfunction_2(){
+  document.getElementById("collapse_1").style.display="none";
+  document.getElementById("three").style.height="3vw";
+  document.getElementById("collapse_2").style.display="block";
+  document.getElementById("collapse_3").style.display="none";
+  document.getElementById("collapse_4").style.display="none";
+  document.getElementById("collapse_5").style.display="none";
+  document.getElementById("player").style.display="none";
+}
+function myfunction_3(){
+  document.getElementById("collapse_1").style.display="none";
+  document.getElementById("three").style.height="3vw";
+  document.getElementById("collapse_2").style.display="none";
+  document.getElementById("collapse_3").style.display="block";
+  document.getElementById("collapse_4").style.display="none";
+  document.getElementById("collapse_5").style.display="none";
+  document.getElementById("player").style.display="none";
+}
+function myfunction_4(){
+  document.getElementById("collapse_1").style.display="none";
+  document.getElementById("three").style.height="3vw";
+  document.getElementById("collapse_2").style.display="none";
+  document.getElementById("collapse_3").style.display="none";
+  document.getElementById("collapse_4").style.display="block";
+  document.getElementById("collapse_5").style.display="none";
+  document.getElementById("player").style.display="none";
+}
+function myfunction_5(){
+  document.getElementById("collapse_1").style.display="none";
+  document.getElementById("three").style.height="3vw";
+  document.getElementById("collapse_2").style.display="none";
+  document.getElementById("collapse_3").style.display="none";
+  document.getElementById("collapse_4").style.display="none";
+  document.getElementById("collapse_5").style.display="block";
+  document.getElementById("player").style.display="none";
+
+}
+function myfunction_7(){
+  document.getElementById("collapse_1").style.display="none";
+  document.getElementById("three").style.height="3vw";
+  document.getElementById("collapse_2").style.display="none";
+  document.getElementById("collapse_3").style.display="none";
+  document.getElementById("collapse_4").style.display="none";
+  document.getElementById("collapse_5").style.display="none";
+  document.getElementById("player").style.display="block";
+}
+function myfunction(){
+  document.getElementById("video").style.display = "block";
+  document.getElementById("player").style.display="none";
+}
